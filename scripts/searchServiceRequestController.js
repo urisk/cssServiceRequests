589d13cbc1821080cd04e4b5696d2616bbc1485c
jsmallwood <jsmallwood@css-wks-joel.eug.cascadesoftwaresystems.com>
2015-12-30 11:34:48 -0800

    initial commit

---
+++ scripts/searchServiceRequestController.js
@@ -0,0 +1,5 @@
+angular.module('cssServiceRequest')
+.controller("newServiceRequestController",['$scope', '$log','WorkRequestCodesService', 
+    function($scope,$log, WorkRequestCodeService){
+    
+}]);
